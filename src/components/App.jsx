eef1d47dbc5b13f4d28971c1ddcb67ac4ce85516
Pablo Armendariz <juanpaarmendariz3@gmail.com>
2023-10-24 19:59:32 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+
+function App() {
+    <div>
+        <Header />
+    </div>
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Header() {
+    <header>
+        <h1>Keeper App</h1>
+    </header>
+}
+
+export default Header;
